dbe48392fac077b2ba2480796a8a8f364bf324ad
Karolina Och-Pudło <okaa93@gmail.com>
2019-10-06 14:03:29 +0200

    Set form arrows depending on the input activity

--- app/js/change-input-arrow.js
+++ app/js/change-input-arrow.js
@@ -1,14 +1,19 @@
 (function() {
 
     function changeArrow() {
-        console.log(this.value);
+        this.classList.toggle('form__input--arrow-up')
+    }
+
+    function resetArrow() {
+        this.classList.remove('form__input--arrow-up')
     }
 
     let selectInputGroup = document.getElementsByClassName('form__input--select');
 
     for (let i=0; i<selectInputGroup.length; i++) {
-        let selectInput = selectInputGroup[0];
+        let selectInput = selectInputGroup[i];
         console.log(selectInput);
         selectInput.addEventListener('click', changeArrow);
+        selectInput.addEventListener('blur', resetArrow);
     }
 }());
